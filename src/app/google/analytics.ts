2ff704d0b165d240bd26448686b795378306c625
Rasmus Ingerslev <rasprivat@hotmail.com>
2026-03-22 14:50:20 +0100

    add google tag v2

--- src/app/google/analytics.ts
+++ src/app/google/analytics.ts
@@ -1,5 +1,17 @@
 export const GA_ID = "AW-18019535668";
 
+type Gtag = (
+    command: "config" | "event",
+    targetId: string,
+    config?: Record<string, any>
+) => void;
+
+declare global {
+    interface Window {
+        gtag?: Gtag;
+    }
+}
+
 export const pageview = (url: string) => {
     window.gtag?.("config", GA_ID, {
         page_path: url,
